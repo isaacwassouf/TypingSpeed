f5443590ebaa7ae4a0b166dc87bd2c49e5ec171e
isaacwassouf <izakwassouf@gmail.com>
2018-02-23 12:42:46 +0200

    fixed Start button clicked while it's already running

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -19,95 +19,92 @@ public class Controller {
     public TextArea fxtArea2;
     private int counter=60;
     private boolean counterisRunning= true;
-
+    private boolean isClicked =true;
     public void startRace(ActionEvent actionEvent) throws Exception {
 
-        String original = setUp();
-        
-        fxtArea.setText(original);
-        Thread keyboard = new Thread(()->{
-            while(counterisRunning) {
-                String input = fxtArea2.getText().toString();
-                int size = input.length();
-                String subOriginal = original.substring(0, size);
-                if (input.equals(subOriginal)) {
-                    Platform.runLater(() -> {
-                        fxlabel.setTextFill(Color.GREEN);
-                    });
-                } else {
-                    Platform.runLater(() -> {
-                        fxlabel.setTextFill(Color.RED);
-                    });
+        if (isClicked) {
+            isClicked= false;
+            String original = setUp();
+
+            fxtArea.setText(original);
+            Thread keyboard = new Thread(() -> {
+                while (counterisRunning) {
+                    String input = fxtArea2.getText().toString();
+                    int size = input.length();
+                    String subOriginal = original.substring(0, size);
+                    if (input.equals(subOriginal)) {
+                        Platform.runLater(() -> {
+                            fxlabel.setTextFill(Color.GREEN);
+                        });
+                    } else {
+                        Platform.runLater(() -> {
+                            fxlabel.setTextFill(Color.RED);
+                        });
+                    }
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                 }
+                Platform.runLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        fxtArea2.setEditable(false);
+                    }
+                });
+
+            });
+
+
+            new Thread(() -> {
+
+                Thread th = new Thread(() -> {
+                    int i = 0;
+                    while (i < 3) {
+                        if (i == 0) {
+                            Platform.runLater(new Runnable() {
+                                @Override
+                                public void run() {
+                                    fxlabel.setText("Ready!");
+                                }
+                            });
+                            i++;
+                        } else if (i == 1) {
+                            Platform.runLater(new Runnable() {
+                                @Override
+                                public void run() {
+                                    fxlabel.setText("Set!");
+                                }
+                            });
+                            i++;
+                        } else if (i == 2) {
+                            Platform.runLater(new Runnable() {
+                                @Override
+                                public void run() {
+                                    fxlabel.setText("Go!");
+                                    fxtArea2.setEditable(true);
+                                }
+                            });
+                            i++;
+                        }
+
+                        try {
+                            Thread.sleep(1000);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                th.start();
                 try {
-                    Thread.sleep(500);
+                    th.join();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-            }
-            Platform.runLater(new Runnable() {
-                @Override
-                public void run() {
-                    fxtArea2.setEditable(false);
-                }
-            });
-
-        });
 
 
-
-        new Thread(() -> {
-
-           Thread th= new Thread(()->{
-              int i=0;
-              while(i < 3){
-                  if (i==0){
-                      Platform.runLater(new Runnable() {
-                          @Override
-                          public void run() {
-                              fxlabel.setText("Ready!");
-                          }
-                      });
-                      i++;
-                  }
-                  else if (i==1){
-                      Platform.runLater(new Runnable() {
-                          @Override
-                          public void run() {
-                              fxlabel.setText("Set!");
-                          }
-                      });
-                      i++;
-                  }
-                  else if (i==2){
-                      Platform.runLater(new Runnable() {
-                          @Override
-                          public void run() {
-                              fxlabel.setText("Go!");
-                              fxtArea2.setEditable(true);
-                          }
-                      });
-                      i++;
-                  }
-
-                  try {
-                      Thread.sleep(1000);
-                  }
-                  catch(Exception e){
-                      e.printStackTrace();
-                  }
-              }
-            });
-            th.start();
-            try {
-                th.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-
-
-            while (counter > 0) {
+                while (counter > 0) {
                     synchronized (new Object()) {
                         counter--;
                     }
@@ -118,29 +115,33 @@ public class Controller {
                         }
                     });
 
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
 
                 }
 
 
-            synchronized (new Object()) {
-                counterisRunning = false;
-            }
-        }).start();
+                synchronized (new Object()) {
+                    counterisRunning = false;
+                }
+                isClicked=true;
+            }).start();
+            keyboard.start();
 
-        keyboard.start();
+        }
+        else{
+            return;
+        }
     }
 
 
     public void stopRace(ActionEvent actionEvent) {
         counterisRunning=false;
         counter=0;
-
-
+        isClicked= true;
     }
 
 
