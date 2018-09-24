8ebc289b33a5061eb660f086aa920b42a2ad42c4
isaacwassouf <izakwassouf@gmail.com>
2018-09-24 21:22:50 +0300

    fixed Start button clicked while it's already running

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,12 +2,20 @@ package sample;
 
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.Random;
 
 
@@ -17,32 +25,72 @@ public class Controller {
     public TextArea fxtArea;
     public Label fxlabel;
     public TextArea fxtArea2;
-    private int counter=60;
-    private boolean counterisRunning= true;
-    private boolean isClicked =true;
+    public Button bid;
+    private  int counter=60;
+    private  boolean counterisRunning= true; // checks if the threads that's counting is running
+    private  boolean isClicked =true; // if the start button is clicked while the threads are running do nothing
+
     public void startRace(ActionEvent actionEvent) throws Exception {
 
         if (isClicked) {
             isClicked= false;
             String original = setUp();
-
             fxtArea.setText(original);
-            Thread keyboard = new Thread(() -> {
+
+            Thread keyboard = new Thread(()  -> {
                 while (counterisRunning) {
                     String input = fxtArea2.getText().toString();
-                    int size = input.length();
-                    String subOriginal = original.substring(0, size);
-                    if (input.equals(subOriginal)) {
-                        Platform.runLater(() -> {
-                            fxlabel.setTextFill(Color.GREEN);
+                    String subOriginal = original.substring(0, input.length());
+
+                    if (input.equals(original)&& counter>=45) {
+                        Platform.runLater(new Runnable() {
+                            @Override
+                            public void run() {
+                                try {
+                                    Pane pane = FXMLLoader.load(getClass().getResource("CopyPasteError.fxml"));
+                                    Stage stage = new Stage();
+                                    stage.setScene(new Scene(pane,508,148));
+                                    stage.show();
+                                    stopRace(null);
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
                         });
+                    } else if(input.equals(original)&& counter<=45){
+                        Platform.runLater(new Runnable() {
+                            @Override
+                            public void run() {
+                                Pane pane = new Pane();
+                                Label label = new Label("Done in "+counter+"s");
+                                label.setLayoutX(190);
+                                label.setLayoutY(50);
+                                label.setStyle("-fx-font: 24 arial;");
+                                pane.getChildren().add(label);
+                                Stage stage =  new Stage();
+                                stage.setScene(new Scene(pane,508,148));
+                                stage.setResizable(false);
+                                stage.show();
+                                stopRace(null);
+                            }
+                        });
+
+
                     } else {
-                        Platform.runLater(() -> {
-                            fxlabel.setTextFill(Color.RED);
-                        });
+                        if (input.equals(subOriginal)) {
+                            Platform.runLater(() -> {
+                                fxlabel.setTextFill(Color.GREEN);
+                            });
+                        } else {
+                            Platform.runLater(() -> {
+                                fxlabel.setTextFill(Color.RED);
+                            });
+                        }
                     }
+
+
                     try {
-                        Thread.sleep(500);
+                        Thread.sleep(100);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
@@ -61,7 +109,7 @@ public class Controller {
 
                 Thread th = new Thread(() -> {
                     int i = 0;
-                    while (i < 3) {
+                    while (i < 3 && counterisRunning ) { // counterisRunning is used to stop the thread if the Stop button was clicked
                         if (i == 0) {
                             Platform.runLater(new Runnable() {
                                 @Override
@@ -132,13 +180,15 @@ public class Controller {
             keyboard.start();
 
         }
+
         else{
             return;
         }
+
     }
 
 
-    public void stopRace(ActionEvent actionEvent) {
+    public  void stopRace(ActionEvent actionEvent) {
         counterisRunning=false;
         counter=0;
         isClicked= true;
@@ -156,17 +206,17 @@ public class Controller {
         int ran= random.nextInt(3);
 
         if (ran==0){
-            fileReader= new FileReader(new File("/Users/isaacwassouf/Desktop/Shaw"));
+            fileReader= new FileReader(new File("/Users/isaacwassouf/Desktop/TypingSpeed/sourceText/Shaw"));
             result =getSource(fileReader);
             fileReader.close();
         }
         else if (ran==1){
-            fileReader= new FileReader(new File("/Users/isaacwassouf/Desktop/RedDialog"));
+            fileReader= new FileReader(new File("/Users/isaacwassouf/Desktop/TypingSpeed/sourceText/RedDialog"));
             result =getSource(fileReader);
             fileReader.close();
         }
         else if (ran==2){
-            fileReader= new FileReader(new File("/Users/isaacwassouf/Desktop/RedDialog2"));
+            fileReader= new FileReader(new File("/Users/isaacwassouf/Desktop/TypingSpeed/sourceText/RedDialog2"));
             result =getSource(fileReader);
             fileReader.close();
         }
@@ -186,6 +236,7 @@ public class Controller {
 
         return result;
     }
+
 
 
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,12 +2,15 @@ package sample;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class Main extends Application {
 
@@ -16,6 +19,8 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("MainGUI.fxml"));
         primaryStage.setTitle("Test your Typing Speed");
         primaryStage.setScene(new Scene(root, 700, 700));
+        //Image image = new Image("/Users/isaacwassouf/Desktop/TypingSpeed/icon.png");
+        //primaryStage.getIcons().add(image);
         primaryStage.show();
 
     }
